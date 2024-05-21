e45dda6fab01ff687be4959c461db36c4220b202
sejyom <akxxkd01@gmail.com>
2024-05-21 09:30:52 +0900

    first commit

---
+++ src/test/java/dmu/cheek/CheekApplicationTests.java
@@ -0,0 +1,13 @@
+package dmu.cheek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
