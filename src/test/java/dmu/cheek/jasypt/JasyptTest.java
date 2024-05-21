85ca8979c37f73fd329ad2d526662ee176cd8f93
sejyom <akxxkd01@gmail.com>
2024-05-21 19:16:59 +0900

    feat: configure Jasypt and Database settings

---
+++ src/test/java/dmu/cheek/jasypt/JasyptTest.java
@@ -0,0 +1,24 @@
+package dmu.cheek.jasypt;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class JasyptTest {
+
+    @Test
+//    @Disabled
+    public void test() {
+        String password = "QhEhwhdk123"; //jasypt password
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPoolSize(4);
+        encryptor.setPassword(password);
+        encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
+
+        String content = "QhEhwhdk123"; //암호화 할 내용
+        String encryptedContent = encryptor.encrypt(content); //암호화
+        String decryptedContent = encryptor.decrypt(encryptedContent); //복호화
+        System.out.println("Enc : " + encryptedContent + ", Dec : " + decryptedContent);
+    }
+}
