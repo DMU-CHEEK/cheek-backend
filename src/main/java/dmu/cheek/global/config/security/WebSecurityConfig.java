f62d5699a8e41adf007228dca9d6b8b9630e8b52
sejyom <akxxkd01@gmail.com>
2024-05-22 14:09:02 +0900

    feat: security config

---
+++ src/main/java/dmu/cheek/global/config/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package dmu.cheek.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(CsrfConfigurer::disable) //CSRF 보호 비활성화
+                .formLogin(AbstractHttpConfigurer::disable) //폼 로그인 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable) //HTTP 기본 인증 비활성화
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 무상태
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/register", "/", "/login").permitAll()
+                        .anyRequest().authenticated())
+                //TODO: jwt 구현 후 삭제하기
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/") //로그아웃 시 메인 화면으로
+                        .invalidateHttpSession(true) //http 세션 무효화
+                );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}
