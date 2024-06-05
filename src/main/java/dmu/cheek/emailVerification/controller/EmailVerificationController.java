11f063eb855cb1d4c989e60b8c5b38dc20ec96bd
sejyom <akxxkd01@gmail.com>
2024-06-05 09:54:59 +0900

    docs: add swagger description

--- src/main/java/dmu/cheek/emailVerification/controller/EmailVerificationController.java
+++ src/main/java/dmu/cheek/emailVerification/controller/EmailVerificationController.java
@@ -2,6 +2,8 @@ package dmu.cheek.emailVerification.controller;
 
 import dmu.cheek.emailVerification.model.EmailVerificationDto;
 import dmu.cheek.emailVerification.service.EmailVerificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/email")
+@Tag(name = "EmailVerification API", description = "이메일 검증 API")
 @Slf4j
 public class EmailVerificationController {
 
     private final EmailVerificationService emailVerificationService;
 
     @PostMapping("/validate-domain")
+    @Operation(summary = "도메인 검증", description = "도메인의 유효성 검증 API")
     public ResponseEntity<Boolean> validateDomain(@RequestParam(name = "domain") String domain) {
         boolean result = emailVerificationService.domainExists(domain);
 
@@ -25,6 +29,7 @@ public class EmailVerificationController {
     }
 
     @PostMapping("/send")
+    @Operation(summary = "이메일 코드 전송", description = "이메일 인증코드 전송 API")
     public ResponseEntity<String> sendAuthCode(@RequestBody EmailVerificationDto emailVerificationRequestDto) throws MessagingException {
         emailVerificationService.registerEmailVerification(emailVerificationRequestDto);
 
@@ -32,6 +37,7 @@ public class EmailVerificationController {
     }
 
     @PostMapping("/verify-code")
+    @Operation(summary = "이메일 코드 검증", description = "이메일 인증코드 검증 API")
     public ResponseEntity<String> verifyEmailCode(@RequestBody EmailVerificationDto emailVerificationDto) {
         emailVerificationService.verifyCode(emailVerificationDto);
 
