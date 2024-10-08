603ee304179d2bd5118d288ce2e1eca4153398d0
sejyom <akxxkd01@gmail.com>
2024-10-08 12:33:46 +0900

    feat: fcm config

---
+++ src/main/java/dmu/cheek/fcm/FCMConfig.java
@@ -0,0 +1,41 @@
+package dmu.cheek.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FCMConfig {
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource("/firebase/cheek-581d2-firebase-adminsdk-yjcxm-81b360d2f9.json");
+
+        InputStream refreshToken = classPathResource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = firebaseApp.getApps();
+
+        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for (FirebaseApp app : firebaseAppList)
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+                    firebaseApp = app;
+        } else {
+            FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+
+            firebaseApp = FirebaseApp.initializeApp(firebaseOptions);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
