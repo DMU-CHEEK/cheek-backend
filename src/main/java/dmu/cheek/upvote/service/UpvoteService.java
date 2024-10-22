20c41038ef79a2615ebf317f167823733f36b14c
sejyom <akxxkd01@gmail.com>
2024-10-22 14:15:01 +0900

    feat: send notification on upvote

--- src/main/java/dmu/cheek/upvote/service/UpvoteService.java
+++ src/main/java/dmu/cheek/upvote/service/UpvoteService.java
@@ -2,6 +2,9 @@ package dmu.cheek.upvote.service;
 
 import dmu.cheek.member.model.Member;
 import dmu.cheek.member.service.MemberService;
+import dmu.cheek.noti.model.Notification;
+import dmu.cheek.noti.model.Type;
+import dmu.cheek.noti.service.NotificationService;
 import dmu.cheek.story.model.Story;
 import dmu.cheek.story.service.StoryService;
 import dmu.cheek.upvote.model.Upvote;
@@ -26,36 +29,54 @@ public class UpvoteService {
     private final UpvoteRepository upvoteRepository;
     private final MemberService memberService;
     private final StoryService storyService;
+    private final NotificationService notificationService;
 
     @Transactional
     public void toggleUpvote(UpvoteDto.Request upvoteDto) {
         Member member = memberService.findById(upvoteDto.getMemberId());
         Story story = storyService.findById(upvoteDto.getStoryId());
 
-        Upvote findUpvote = upvoteRepository.findByStoryAndMember(member, story)
+        Upvote upvote = upvoteRepository.findByStoryAndMember(member, story)
                 .orElse(null);
 
-        if (findUpvote == null) {
-            upvoteRepository.save(
-                    Upvote.withoutPrimaryKey()
-                            .member(member)
-                            .story(story)
-                            .isUpvoted(true)
+        boolean isNewUpvote = false;
+
+        if (upvote == null) {
+            //좋아요 신규 등록
+            upvote = Upvote.withoutPrimaryKey()
+                    .member(member)
+                    .story(story)
+                    .isUpvoted(true)
+                    .build();
+
+            isNewUpvote = true;
+
+            upvoteRepository.save(upvote);
+
+            log.info("register upvote, storyId: {}", story.getStoryId());
+        } else {
+            //좋아요 토글링
+            boolean currentStatus = upvote.isUpvoted();
+            upvote.toggleUpvote(!currentStatus);
+            log.info("storyId: {}, upvote status: {}", story.getStoryId(), upvote.isUpvoted());
+        }
+
+        if (isNewUpvote || upvote.isUpvoted()) {
+            //send notification
+            String notiBody = String.format("%s님이 내 스토리에 좋아요를 표시했어요.", upvote.getMember().getNickname());
+
+            notificationService.register(
+                    Notification.withoutPrimaryKey()
+                            .type(Type.from("UPVOTE"))
+                            .typeId(upvote.getUpvoteId())
+                            .toMember(upvote.getStory().getMember())
+                            .fromMember(upvote.getMember())
+                            .body(notiBody)
                             .build()
             );
-            log.info("register upvote, storyId: {}", story.getStoryId());
-        }
-
-        else {
-            if (!findUpvote.isUpvoted())
-                findUpvote.toggleUpvote(true);
-
-            else
-                findUpvote.toggleUpvote(false);
-
-            log.info("storyId: {}, upvote status: {}", story.getStoryId(), findUpvote.isUpvoted());
         }
     }
+
 
     public Optional<Upvote> findByMemberId(Long id) {
         return upvoteRepository.findByMemberId(id);
