e4c1a9637c58a4cec95f9362dbee3ea0dd47593f
sejyom <akxxkd01@gmail.com>
2024-11-06 10:16:37 +0900

    add @Modifying annotation to jpql query

--- src/main/java/dmu/cheek/noti/repository/NotificationRepository.java
+++ src/main/java/dmu/cheek/noti/repository/NotificationRepository.java
@@ -2,6 +2,7 @@ package dmu.cheek.noti.repository;
 
 import dmu.cheek.noti.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     @Query("select n from Notification n where n.toMember.memberId = :memberId")
     List<Notification> findByToMemberId(long memberId);
 
-    @Query("select n from Notification n where n.toMember.memberId = :memberId")
+    @Modifying
+    @Query("delete from Notification n where n.toMember.memberId = :memberId")
     void deleteAllByToMemberId(long memberId);
 }
